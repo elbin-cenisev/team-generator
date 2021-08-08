721bce9e438ef3b3d3378790be60c2dd6cf3eacc
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-08 18:56:00 -0400

    Created arrays for each set of questions

--- index.js
+++ index.js
@@ -10,3 +10,39 @@ const writeFileAsync = util.promisify(fs.writeFile);
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+// Question arrays
+const commonQuestions = [
+    {
+        name: 'name',
+        message: 'What is their name?'
+    },
+    {
+        name: 'id',
+        message: 'What is their employee ID number?'
+    },
+    {
+        name: 'email',
+        message: 'What is their email address?'
+    }
+];
+const managerQuestions = commonQuestions.concat({
+    name: 'officeNumber',
+    message: 'What is their office number?'
+});
+const internQuestions = commonQuestions.concat({
+    name: 'school',
+    message: 'What school do they attend?'
+});
+const engineerQuestions = commonQuestions.concat({
+    name: 'github',
+    message: 'What is their github username?'
+});
+const addEmployeeQuestion = [
+    {
+        name: 'addEmployee',
+        type: 'list',
+        message: 'Who would you like to add to the team?',
+        choices: ["Engineer", "Intern", "Nobody"]
+    }
+];
